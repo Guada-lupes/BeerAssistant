4dce24c60cbdfa58acc2daef5fb3060f9f9aa895
afuedia <cholastal@gmail.com>
2025-04-05 20:24:47 +0100

    creo objeto con descripciones BJCP de colores, numeros SRC y colores hexadecimales correspondientes. creo objeto con categorias y estilos. Creo un objeto ipa.jsx como ejemplo de como podría ser

---
+++ src/services/colores.jsx
@@ -0,0 +1,34 @@
+const beerColors = [
+  { descripcion: "Pajizo", numeroSRM: [2, 3], color: "#F4F1BB" },
+  { descripcion: "Amarillo", numeroSRM: [3, 4], color: "#F8E667" },
+  { descripcion: "Dorado", numeroSRM: [5, 6], color: "#F1C40F" },
+  { descripcion: "Ámbar", numeroSRM: [6, 7, 8, 9], color: "#D98A1C" },
+  {
+    descripcion: "Ámbar profundo",
+    numeroSRM: [10, 11, 12, 13, 14],
+    color: "#B5651D",
+  },
+  { descripcion: "Cobre", numeroSRM: [14, 15, 16, 17], color: "#A0522D" },
+  { descripcion: "Cobre profundo", numeroSRM: [17, 18], color: "#8B4513" },
+  { descripcion: "Marrón", numeroSRM: [19, 20, 21, 22], color: "#6F4E37" },
+  {
+    descripcion: "Marrón oscuro",
+    numeroSRM: [22, 23, 24, 25, 26, 27, 28, 29, 30],
+    color: "#4B3621",
+  },
+  {
+    descripcion: "Marrón muy oscuro",
+    numeroSRM: [30, 31, 32, 33, 34, 35],
+    color: "#3B2F2F",
+  },
+  {
+    descripcion: "Negro",
+    numeroSRM: [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+    color: "#1C1C1C",
+  },
+  {
+    descripcion: "Negro opaco",
+    numeroSRM: [40, 41, 42, 43, 44],
+    color: "#000000",
+  },
+];
